990596dbc1e96b479179832525e78f3de9d5407f
nina.alvarezdemalde <alvarezdemalde.nina@gmail.com>
2025-11-04 15:34:10 -0500

    hopefully wrote correct auto lol

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueAuto.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name="Blue Auto Test")
+@Autonomous(name="Blue Auto Field")
 public class BlueAuto extends LinearOpMode {
 
     RobotBase robot;
