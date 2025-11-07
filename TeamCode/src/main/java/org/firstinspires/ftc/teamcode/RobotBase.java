d8fc9337e5c09576abc5a8b687c4fd6c40354241
Ana Lisa <157230583+AnaLisaVigilBenitez@users.noreply.github.com>
2025-11-07 16:52:52 -0500

    #changed the timer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotBase.java
@@ -12,7 +12,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 public class RobotBase extends DriveBase{
 
     //TODO: from carter - remember because changes
-    final double FEED_TIME_SECONDS = 0.40; //The feeder servos run this long when a shot is requested.
+    final double FEED_TIME_SECONDS = 1.20; //The feeder servos run this long when a shot is requested.
     final double STOP_SPEED = 0.0; //We send this power to the servos when we want them to stop.
     final double FULL_SPEED = 1.0;
     final double LAUNCHDURATION_SECONDS = 2.0; //The amount of time to wait before turning off the flywheel
@@ -47,7 +47,7 @@ public class RobotBase extends DriveBase{
         intake1_2 = myOpMode.hardwareMap.get(DcMotor.class,"intake1_2");
 
         //feeder intake is prob wrong
-        feeder.setDirection(DcMotorSimple.Direction.REVERSE); //previous crservo
+        feeder.setDirection(CRServo.Direction.REVERSE); //previous crservo
         flyWheel.setDirection(DcMotor.Direction.FORWARD);
         intake1_2.setDirection(DcMotor.Direction.FORWARD);
     }
