b757014ae133cd0a4081126ebbcf7782f3e12565
sadie.markowicz <sadie.markowicz@pinecrest.edu>
2025-11-10 17:33:38 -0500

    changed auto duration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotBase.java
@@ -12,7 +12,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 public class RobotBase extends DriveBase{
 
     //TODO: from carter - remember because changes
-    final double FEED_TIME_SECONDS = 0.40; //The feeder servos run this long when a shot is requested.
+    final double FEED_TIME_SECONDS = 2.0; //The feeder servos run this long when a shot is requested.
     final double STOP_SPEED = 0.0; //We send this power to the servos when we want them to stop.
     final double FULL_SPEED = 1.0;
     final double LAUNCHDURATION_SECONDS = 2.0; //The amount of time to wait before turning off the flywheel
