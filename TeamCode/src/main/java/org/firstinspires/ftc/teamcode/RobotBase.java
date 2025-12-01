1c642231e5b244886f97bf684d632690e231ff6e
Ana Lisa <157230583+AnaLisaVigilBenitez@users.noreply.github.com>
2025-12-01 16:23:06 -0500

    fixed stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotBase.java
@@ -25,8 +25,8 @@ public class RobotBase extends DriveBase{
      */
 
     //TODO: change launch velocities for code to actually work
-    final double LAUNCHER_TARGET_VELOCITY = 1525; //flyWheel
-    final double LAUNCHER_MIN_VELOCITY = 1425; //starts feeder
+    final double LAUNCHER_TARGET_VELOCITY = 1325; //flyWheel
+    final double LAUNCHER_MIN_VELOCITY = 1225; //starts feeder
 
     ElapsedTime feederTimer = new ElapsedTime();
     ElapsedTime StopTimer = new ElapsedTime();
